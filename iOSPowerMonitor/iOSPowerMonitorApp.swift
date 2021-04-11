037d646579edd0e3edddf48a59da79d9f6dca338
alexisdevelops <alexisdevelops@gmail.com>
2021-04-11 15:47:10 -0700

    Initial Commit

---
+++ iOSPowerMonitor/iOSPowerMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSPowerMonitorApp.swift
+//  iOSPowerMonitor
+//
+//  Created by alexis on 11/04/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSPowerMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
